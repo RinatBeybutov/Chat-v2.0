8371c8e9545b86f8baafb1ad7615884a76af6783
Ринат <ignat934@gmail.com>
2025-03-20 20:30:55 +0300

    16.03 - Написали методы репозиторий - Добавили юнит тесты для репозитория - Добавили скрипт проверки тестов (CI)

---
+++ src/test/java/com/maxima/chat/config/PostgresDbTestcontainers.java
@@ -0,0 +1,28 @@
+package com.maxima.chat.config;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * Конфигурационный класс для контейнера PostgreSQL
+ */
+@Testcontainers
+public class PostgresDbTestcontainers {
+
+  @Container
+  @ServiceConnection
+  static final PostgreSQLContainer<?> postgresContainer =
+      new PostgreSQLContainer<>("postgres:15-alpine");
+
+  @DynamicPropertySource
+  static void setProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+    registry.add("spring.datasource.username", postgresContainer::getUsername);
+    registry.add("spring.datasource.password", postgresContainer::getPassword);
+  }
+
+}
